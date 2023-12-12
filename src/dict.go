3a54b9999d128efe5ba1788a429898648c625e82
luoyang <1193055746@qq.com>
2023-12-12 17:47:19 +0800

    update: 完善dict方法，实现rehash

--- src/dict.go
+++ src/dict.go
@@ -2,17 +2,20 @@ package src
 
 import (
 	"hash/fnv"
+	"math"
 	"simple-redis/utils"
 )
 
+// LOAD_FACTOR 负载因子
+// BG_PERSISTENCE_LOAD_FACTOR bgsave或者bgrewriteaof 的负载因子
 const (
 	DICK_OK                    = 0
 	DICK_ERR                   = 1
 	DEFAULT_REHASH_STEP        = 1
-	DICT_HT_INITIAL_SIZE       = 4
-	EXPEND_RATIO               = 2
+	DICT_HT_INITIAL_SIZE int64 = 4
+	EXPEND_RATIO         int64 = 2
 	LOAD_FACTOR                = 1
-	BG_PERSISTENCE_LOAD_FACTOR = 5
+	//BG_PERSISTENCE_LOAD_FACTOR       = 5
 )
 
 type dictEntry struct {
@@ -76,20 +79,88 @@ func dictCreate(dType *dictType) *dict {
 	return d
 }
 
+func freeDictEntry(e *dictEntry) {
+	e.key.decrRefCount()
+	e.val.decrRefCount()
+}
+
 func (d *dict) isRehash() bool {
 	return d.rehashIdx != -1
 }
 
 func (d *dict) dictRehash(step int) {
-	// TODO
+	if !d.isRehash() {
+		return
+	}
+	for ; step > 0; step-- {
+		// Check if we already rehashed the whole table
+		if d.ht[0].used == 0 {
+			d.ht[0] = d.ht[1]
+			d.ht[1] = nil
+			d.rehashIdx = -1
+			return
+		}
+		// find a not nil elem
+		rehashNoNullStep := server.rehashNullStep
+		if rehashNoNullStep > (d.ht[0].size - d.rehashIdx) {
+			rehashNoNullStep = d.ht[0].size - d.rehashIdx
+		}
+		for ; rehashNoNullStep > 0; rehashNoNullStep-- {
+			if d.ht[0].table[d.rehashIdx] == nil {
+				d.rehashIdx++
+			}
+		}
+		entry := d.ht[0].table[d.rehashIdx]
+		// cannot find a not nil elem
+		if entry == nil {
+			return
+		}
+		for entry != nil {
+			nextEntry := entry.next
+			h := d.dType.hashFunc(entry.key) & d.ht[1].mask
+			entry.next = d.ht[1].table[h]
+			d.ht[1].table[h] = entry
+			d.ht[0].used--
+			d.ht[1].used++
+			entry = nextEntry
+		}
+		d.ht[0].table[d.rehashIdx] = nil
+		d.rehashIdx++
+	}
 }
 
 func (d *dict) dictRehashStep() {
 	d.dictRehash(DEFAULT_REHASH_STEP)
 }
 
+func (d *dict) dictNextPower(size int64) int64 {
+	i := DICT_HT_INITIAL_SIZE
+	if size > math.MaxInt64 {
+		return math.MaxInt64
+	}
+	for {
+		if i >= size {
+			return i
+		}
+		i *= EXPEND_RATIO
+	}
+}
+
 func (d *dict) dictExpand(size int64) int {
-	// TODO
+	realSize := d.dictNextPower(size)
+
+	if d.isRehash() || d.ht[0].used > size {
+		return DICK_ERR
+	}
+
+	ht := new(dictht)
+	ht.used = 0
+	ht.size = realSize
+	ht.mask = realSize - 1
+	ht.table = make([]*dictEntry, realSize)
+
+	d.ht[1] = ht
+	d.rehashIdx = 0
 	return DICK_OK
 }
 
@@ -97,7 +168,7 @@ func (d *dict) dictExpandIfNeeded() int {
 	if d.isRehash() {
 		return DICK_OK
 	}
-	if d.ht[0].used > d.ht[0].size && (d.ht[0].used/d.ht[0].size > server.loadFactor) {
+	if d.ht[0].used > d.ht[0].size && (float64(d.ht[0].used)/float64(d.ht[0].size) > float64(server.loadFactor)) {
 		return d.dictExpand(d.ht[0].size * EXPEND_RATIO)
 	}
 	return DICK_OK
@@ -108,31 +179,38 @@ func (d *dict) dictKeyIndex(key *SRobj) int64 {
 	if err := d.dictExpandIfNeeded(); err != DICK_OK {
 		return -1
 	}
+	idx, _ := d.dictFind(key)
+	return idx
+}
+
+func (d *dict) dictFind(key *SRobj) (int64, *dictEntry) {
+	if d.ht[0].size == 0 {
+		return -1, nil
+	}
+	if d.isRehash() {
+		d.dictRehashStep()
+	}
 	h := d.dType.hashFunc(key)
 	var idx int64
-	for table := 0; table < 1; table++ {
+	for table := 0; table <= 1; table++ {
 		idx = h & d.ht[table].mask
 		he := d.ht[table].table[idx]
 		for he != nil {
 			if d.dType.keyCompare(key, he.key) {
-				return -1
+				return -1, he
 			}
 			he = he.next
 		}
-		if d.isRehash() {
+		if !d.isRehash() {
 			break
 		}
 	}
-	return idx
+	return idx, nil
 }
 
 func (d *dict) dictAddRaw(key *SRobj) *dictEntry {
-	if d.isRehash() {
-		d.dictRehashStep()
-	}
-
 	var idx int64
-	var entry *dictEntry
+	var entry dictEntry
 	var ht *dictht
 	if idx = d.dictKeyIndex(key); idx == -1 {
 		return nil
@@ -144,9 +222,9 @@ func (d *dict) dictAddRaw(key *SRobj) *dictEntry {
 	entry.key = key
 	entry.next = ht.table[idx]
 	key.incrRefCount()
-	ht.table[idx] = entry
+	ht.table[idx] = &entry
 	ht.used++
-	return entry
+	return &entry
 }
 
 func (d *dict) dictAdd(key, val *SRobj) int {
@@ -157,4 +235,58 @@ func (d *dict) dictAdd(key, val *SRobj) int {
 	entry.val = val
 	val.incrRefCount()
 	return DICK_OK
+}
+
+func (d *dict) dictSet(key, val *SRobj) {
+	if d.dictAdd(key, val) == DICK_OK {
+		return
+	}
+	_, entry := d.dictFind(key)
+	entry.val.decrRefCount()
+	entry.val = val
+	entry.val.incrRefCount()
+}
+
+func (d *dict) dictGet(key *SRobj) *SRobj {
+	_, entry := d.dictFind(key)
+	if entry == nil {
+		return nil
+	}
+	return entry.val
+}
+
+func (d *dict) dictDelete(key *SRobj) int {
+	if d.ht[0].size == 0 {
+		return DICK_ERR
+	}
+
+	if d.isRehash() {
+		d.dictRehashStep()
+	}
+
+	h := d.dType.hashFunc(key)
+	var idx int64
+	for table := 0; table <= 1; table++ {
+		idx = h & d.ht[table].mask
+		he := d.ht[table].table[idx]
+		var preHe *dictEntry
+		for he != nil {
+			if d.dType.keyCompare(key, he.key) {
+				if preHe == nil {
+					d.ht[table].table[idx] = he.next
+				} else {
+					preHe.next = he.next
+				}
+				freeDictEntry(he)
+				d.ht[table].used--
+				return DICK_OK
+			}
+			preHe = he
+			he = he.next
+		}
+		if !d.isRehash() {
+			break
+		}
+	}
+	return DICK_ERR
 }
---
+++ src/dict_test.go
@@ -0,0 +1,196 @@
+package src
+
+import (
+	"testing"
+)
+
+func TestSRStrHash(t *testing.T) {
+	str := createSRobj(SR_STR, "name")
+	ints := createSRobj(SR_DICT, 1)
+	h1 := SRStrHash(ints)
+	if h1 != 0 {
+		t.Error("SRStrHash err: h == ", h1)
+	}
+	if SRStrHash(str) != SRStrHash(str) {
+		t.Error("SRStrHash err")
+	}
+}
+
+func TestSRStrCompare(t *testing.T) {
+	str1 := createSRobj(SR_STR, "name")
+	str2 := createSRobj(SR_STR, "name1")
+	res := SRStrCompare(str1, str2)
+	if res {
+		t.Error("SRStrCompare err: res == ", res)
+	}
+}
+
+func TestDictCreate(t *testing.T) {
+	d := dictCreate(&dictType{hashFunc: SRStrHash, keyCompare: SRStrCompare})
+	if d.rehashIdx != -1 {
+		t.Error("dict init rehashidx err: rehashidx == ", d.rehashIdx)
+	}
+	if d.ht[0].size != DICT_HT_INITIAL_SIZE {
+		t.Error("dict init ht size err: size == ", d.ht[0].size)
+	}
+}
+
+func TestFreeDictEntry(t *testing.T) {
+	key := createSRobj(SR_STR, "name")
+	val := createSRobj(SR_STR, "ly")
+	e := new(dictEntry)
+	e.key = key
+	e.key.incrRefCount()
+	e.val = val
+	e.val.incrRefCount()
+	if e.key.refCount != 2 || e.val.refCount != 2 {
+		t.Errorf("incrRefCount err: key = %d, val = %d", e.key.refCount, e.val.refCount)
+	}
+	freeDictEntry(e)
+	if e.key.refCount != 1 || e.val.refCount != 1 {
+		t.Errorf("incrRefCount err: key = %d, val = %d", e.key.refCount, e.val.refCount)
+	}
+}
+
+func TestIsRehash(t *testing.T) {
+	d := dictCreate(&dictType{hashFunc: SRStrHash, keyCompare: SRStrCompare})
+	if d.isRehash() {
+		t.Error("isRehash err: d.isRehash == ", d.isRehash())
+	}
+	d.rehashIdx = 0
+	if !d.isRehash() {
+		t.Error("isRehash err: d.isRehash == ", d.isRehash())
+	}
+}
+
+func TestDictRehash(t *testing.T) {
+	server.rehashNullStep = 10
+	server.loadFactor = LOAD_FACTOR
+	d := dictCreate(&dictType{hashFunc: SRStrHash, keyCompare: SRStrCompare})
+	d.dictSet(createSRobj(SR_STR, "name"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name1"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name2"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name3"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name4"), createSRobj(SR_STR, "ly"))
+	if d.isRehash() {
+		t.Error("isRehash err: d.isRehash == ", d.isRehash())
+	}
+	d.dictExpandIfNeeded()
+	d.dictRehash(1)
+	d.dictRehash(1)
+	d.dictRehash(1)
+	d.dictRehash(1)
+	d.dictRehash(1)
+	if d.isRehash() {
+		t.Error("isRehash err: d.isRehash == ", d.isRehash())
+	}
+}
+
+func TestDictNextPower(t *testing.T) {
+	d := dictCreate(&dictType{hashFunc: SRStrHash, keyCompare: SRStrCompare})
+	s1 := d.dictNextPower(5)
+	if s1 != 8 {
+		t.Error("dictNextPower err: size == ", s1)
+	}
+}
+
+func TestDictExpand(t *testing.T) {
+	server.rehashNullStep = 10
+	server.loadFactor = LOAD_FACTOR
+	d := dictCreate(&dictType{hashFunc: SRStrHash, keyCompare: SRStrCompare})
+	d.dictSet(createSRobj(SR_STR, "name"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name1"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name2"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name3"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name4"), createSRobj(SR_STR, "ly"))
+	d.dictExpand(5)
+	if d.ht[1].size != 8 {
+		t.Error("dictExpand err: size == ", d.ht[1].size)
+	}
+}
+
+func TestDictKeyIndex(t *testing.T) {
+	d := dictCreate(&dictType{hashFunc: SRStrHash, keyCompare: SRStrCompare})
+	key := createSRobj(SR_STR, "name")
+	val := createSRobj(SR_STR, "ly")
+	d.dictSet(key, val)
+	idx := d.dictKeyIndex(key)
+	if idx != -1 {
+		t.Errorf("dictKeyIndex err: idx = %d", idx)
+	}
+	idx = d.dictKeyIndex(createSRobj(SR_STR, "name1"))
+	if idx == -1 {
+		t.Errorf("dictKeyIndex err: idx = %d", idx)
+	}
+}
+
+func TestDictFind(t *testing.T) {
+	d := dictCreate(&dictType{hashFunc: SRStrHash, keyCompare: SRStrCompare})
+	key := createSRobj(SR_STR, "name")
+	val := createSRobj(SR_STR, "ly")
+	d.dictSet(key, val)
+	idx, e := d.dictFind(key)
+	if idx != -1 {
+		t.Errorf("dictFind err: idx = %d", idx)
+	}
+	if val.strVal() != e.val.strVal() {
+		t.Errorf("dictFind err: val = %s", d.ht[0].table[idx].val.strVal())
+	}
+}
+
+func TestDictSet(t *testing.T) {
+	server.rehashNullStep = 10
+	server.loadFactor = LOAD_FACTOR
+	d := dictCreate(&dictType{hashFunc: SRStrHash, keyCompare: SRStrCompare})
+	d.dictSet(createSRobj(SR_STR, "name"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name1"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name2"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name3"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name4"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name5"), createSRobj(SR_STR, "ly"))
+	if !d.isRehash() {
+		t.Error("dictSet err")
+	}
+	d.dictGet(createSRobj(SR_STR, "name"))
+	d.dictGet(createSRobj(SR_STR, "name"))
+	d.dictGet(createSRobj(SR_STR, "name"))
+	d.dictGet(createSRobj(SR_STR, "name"))
+	if d.isRehash() {
+		t.Error("dictSet err")
+	}
+}
+
+func TestDictGet(t *testing.T) {
+	d := dictCreate(&dictType{hashFunc: SRStrHash, keyCompare: SRStrCompare})
+	key := createSRobj(SR_STR, "name")
+	val := createSRobj(SR_STR, "ly")
+	d.dictSet(key, val)
+	v := d.dictGet(key)
+	if v.strVal() != val.strVal() {
+		t.Error("dictGet err")
+	}
+	v = d.dictGet(createSRobj(SR_STR, "name1"))
+	if v != nil {
+		t.Error("dictGet err")
+	}
+}
+
+func TestDictDelete(t *testing.T) {
+	d := dictCreate(&dictType{hashFunc: SRStrHash, keyCompare: SRStrCompare})
+	key := createSRobj(SR_STR, "name")
+	val := createSRobj(SR_STR, "ly")
+	d.dictSet(key, val)
+	d.dictSet(createSRobj(SR_STR, "name1"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name2"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name3"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name4"), createSRobj(SR_STR, "ly"))
+	d.dictSet(createSRobj(SR_STR, "name5"), createSRobj(SR_STR, "ly"))
+
+	if d.dictGet(key).strVal() != val.strVal() {
+		t.Error("dictGet err")
+	}
+	d.dictDelete(key)
+	if d.dictGet(key) != nil {
+		t.Error("dictDelete err")
+	}
+}
