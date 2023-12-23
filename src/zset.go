4127c72d9d1eac00cbd979f49e27aa957d3079b9
luoyang <1193055746@qq.com>
2023-12-23 14:03:53 +0800

    add: zset skiplist 结构

---
+++ src/zset.go
@@ -0,0 +1,22 @@
+package src
+
+type zSkipListNode struct {
+	obj            *SRobj
+	score          float64
+	backward       *zSkipListNode
+	zSkipListLevel struct {
+		forward *zSkipListNode
+		span    uint
+	}
+}
+
+type zSkipList struct {
+	header, tail *zSkipListNode
+	length       uint64
+	level        int
+}
+
+type zSet struct {
+	zsl *zSkipList
+	d   *dict
+}
