045fb9f32eef287b18acb99ccbce111f0b44e58c
luoyang <1193055746@qq.com>
2023-12-26 13:17:51 +0800

    update: zslInsert

--- src/zset.go
+++ src/zset.go
@@ -1,22 +1,149 @@
 package src
 
+import (
+	"math/rand"
+)
+
+const (
+	ZSKIPLIST_MAXLEVEL = 32
+	ZSKIPLIST_P        = 0.25
+)
+
+type zSkipListNodeLevel struct {
+	forward *zSkipListNode
+	span    uint
+}
+
 type zSkipListNode struct {
 	obj      *SRobj
 	score    float64
 	backward *zSkipListNode
-	level    []struct {
-		forward *zSkipListNode
-		span    uint
-	}
+	level    []*zSkipListNodeLevel
+}
+
+func (zn *zSkipListNode) freeNode() {
+	zn.obj.decrRefCount()
+	zn.obj = nil
+	zn.backward = nil
+	zn.level = nil
 }
 
 type zSkipList struct {
 	header, tail *zSkipListNode
-	length       uint64
+	length       uint
 	level        int
+}
+
+func (z *zSkipList) free() {
+	var next *zSkipListNode
+	node := z.header.level[0].forward
+	// free
+	z.header = nil
+	for node != nil {
+		next = node.level[0].forward
+		node.freeNode()
+		node = next
+	}
+	z.tail = nil
+}
+
+func (z *zSkipList) zslInsert(score float64, obj *SRobj) *zSkipListNode {
+	var update [ZSKIPLIST_MAXLEVEL]*zSkipListNode
+	var rank [ZSKIPLIST_MAXLEVEL]uint
+	var x *zSkipListNode
+	var i, level int
+
+	x = z.header
+	for i = z.level - 1; i >= 0; i-- {
+		rank[i] = 0
+		if i != (z.level - 1) {
+			rank[i] = rank[i+1]
+		}
+
+		for x.level[i].forward != nil &&
+			(x.level[i].forward.score < score ||
+				(x.level[i].forward.score == score &&
+					compareStringObjects(x.level[i].forward.obj, obj) < 0)) {
+			rank[i] += x.level[i].span
+			x = x.level[i].forward
+		}
+		update[i] = x
+	}
+
+	level = zslRandomLevel()
+	if level > z.level {
+		for i = z.level; i < level; i++ {
+			rank[i] = 0
+			update[i] = z.header
+			update[i].level[i].span = z.length
+		}
+		z.level = level
+	}
+	x = zslCreateNode(level, score, obj)
+	for i = 0; i < level; i++ {
+		x.level[i].forward = update[i].level[i].forward
+		update[i].level[i].forward = x
+
+		x.level[i].span = update[i].level[i].span - (rank[0] - rank[i])
+		update[i].level[i].span = (rank[0] - rank[i]) + 1
+	}
+
+	for i = level; i < z.level; i++ {
+		update[i].level[i].span++
+	}
+
+	x.backward = update[0]
+	if update[0] == z.header {
+		x.backward = nil
+	}
+
+	if x.level[0].forward != nil {
+		x.level[0].forward.backward = x
+	} else {
+		z.tail = x
+	}
+
+	z.length++
+	return x
 }
 
 type zSet struct {
 	zsl *zSkipList
 	d   *dict
+}
+
+func zslRandomLevel() int {
+	level := 1
+	for float64(rand.Int63()&0xFFFF) < (ZSKIPLIST_P * 0xFFFF) {
+		level++
+	}
+	if level > ZSKIPLIST_MAXLEVEL {
+		level = ZSKIPLIST_MAXLEVEL
+	}
+	return level
+}
+
+func zslCreateNode(level int, score float64, obj *SRobj) *zSkipListNode {
+	zsln := new(zSkipListNode)
+	zsln.obj = obj
+	zsln.score = score
+	zsln.level = make([]*zSkipListNodeLevel, level)
+	for i := 0; i < level; i++ {
+		zsln.level[i] = new(zSkipListNodeLevel)
+	}
+	return zsln
+}
+
+func zslCreate() *zSkipList {
+	zsl := new(zSkipList)
+	zsl.length = 0
+	zsl.level = 1
+	zsl.header = zslCreateNode(ZSKIPLIST_MAXLEVEL, 0, nil)
+	for i := 0; i < ZSKIPLIST_MAXLEVEL; i++ {
+		zsl.header.level[i].forward = nil
+		zsl.header.level[i].span = 0
+	}
+	zsl.header.backward = nil
+	zsl.tail = nil
+	return zsl
 }
---
+++ src/zset_test.go
@@ -0,0 +1,50 @@
+package src
+
+import (
+	"testing"
+)
+
+func TestZslCreateNode(t *testing.T) {
+	str := "qqqq"
+	score := 1.2
+	zn := zslCreateNode(7, 1.2, createSRobj(SR_STR, "qqqq"))
+	if zn.obj.strVal() != str {
+		t.Error("zslCreateNode err: zn.obj = ", zn.obj.strVal())
+	}
+	if zn.score != score {
+		t.Error("zslCreateNode err: score = ", score)
+	}
+	if len(zn.level) != 7 {
+		t.Error("zslCreateNode err: len(zn.level) = ", len(zn.level))
+	}
+}
+
+func TestZslCreate(t *testing.T) {
+	zsl := zslCreate()
+	if len(zsl.header.level) != ZSKIPLIST_MAXLEVEL {
+		t.Error("zslCreate err: len(zsl.header.level) = ", len(zsl.header.level))
+	}
+	if zsl.length != 0 {
+		t.Error("zslCreate err: zsl.length = ", zsl.length)
+	}
+	if zsl.level != 1 {
+		t.Error("zslCreate err: zsl.level = ", zsl.level)
+	}
+}
+
+func TestZslInsert(t *testing.T) {
+	zsl := zslCreate()
+	o1 := createSRobj(SR_STR, "qqqq")
+	zsl.zslInsert(5.1, o1)
+	o2 := createSRobj(SR_STR, "qqq")
+	zsl.zslInsert(4.6, o2)
+	o3 := createSRobj(SR_STR, "qqq11")
+	zsl.zslInsert(4.8, o3)
+	o4 := createSRobj(SR_STR, "qqq112")
+	zsl.zslInsert(4.7, o4)
+	o5 := createSRobj(SR_STR, "qqq1122")
+	zsl.zslInsert(5.0, o5)
+	if zsl.length != 5 {
+		t.Error("zslInsert err: zsl.length = ", zsl.length)
+	}
+}
